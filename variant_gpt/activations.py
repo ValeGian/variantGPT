68d9dd91eba68202c04df6606e8c3044eb4b6ce6
ValeGian <46gianninivalerio@gmail.com>
2025-08-27 21:57:09 +0000

    Add basic activations

---
+++ variant_gpt/activations.py
@@ -0,0 +1,34 @@
+import logging
+from collections import OrderedDict
+
+from torch import nn
+
+logger = logging.get_logger(__name__)
+
+
+class ClassInstantier(OrderedDict):
+    def __getitem__(self, key):
+        content = super().__getitem__(key)
+        cls, kwargs = content if isinstance(content, tuple) else (content, {})
+        return cls(**kwargs)
+
+
+ACT2CLS = {
+    "gelu": nn.GELU,
+    "leaky_relu": nn.LeakyReLU,
+    "relu": nn.ReLU,
+    "relu6": nn.ReLU6,
+    "sigmoid": nn.Sigmoid,
+    "silu": nn.SiLU,
+    "swish": nn.SiLU,
+    "tanh": nn.Tanh,
+    "prelu": nn.PReLU,
+}
+ACT2FN = ClassInstantier(ACT2CLS)
+
+
+def get_activation(activation_string):
+    if activation_string in ACT2FN:
+        return ACT2FN[activation_string]
+    else:
+        raise KeyError(f"function {activation_string} not found in ACT2FN mapping {list(ACT2FN.keys())}")
