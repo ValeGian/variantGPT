88e664a57c084d94847fd411c9b5cd42ff77e659
v.giannini <46gianninivalerio@gmail.com>
2026-04-25 23:02:27 +0200

    Make it so that train.sh takes params from CLI

--- pretrain/train.sh
+++ pretrain/train.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export ***REMOVED***
-export ***REMOVED***
-export ***REMOVED***
-export ***REMOVED***
-
 export NCCL_SOCKET_IFNAME=bond0
 export GLOO_SOCKET_IFNAME=bond0
 export MASTER_ADDR=10.40.1.8
@@ -18,5 +13,4 @@ torchrun \
   --master_port=29500 \
   --local-addr=10.40.1.8 \
   train.py \
-  --run_name "mha" \
-  --attention_type "mha"
+  "$@"
